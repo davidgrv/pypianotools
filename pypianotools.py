fa6b346d36344679e5862ba90413e51736942ae1
David G <david.garaev@mail.utoronto.ca>
2024-01-22 16:56:53 -0500

    Changed language to be more concise when referring to piano key vs. note. Made reused variables note_names and note_mapping global.

--- pypianotools.py
+++ pypianotools.py
@@ -1,9 +1,11 @@
 import math
 
+note_names = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+note_mapping = {'C': 0, 'C#': 1, 'D': 2, 'D#': 3, 'E': 4, 'F': 5, 'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10, 'B': 11}
+
 
 def freq_to_note(freq):
     key_num = round(12 * (math.log2(freq / 440)) + 49)
-    note_names = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
     oct_num = (key_num - 1) // 12
     note_index = (key_num - 1) % 12
     note = f'{note_names[note_index]}{oct_num}'
@@ -12,7 +14,7 @@ def freq_to_note(freq):
 
 
 def note_to_freq(note, decimal_places=3):
-    a4_freq = 440.0
+    a4_freq = 440.000
     key_mapping = {
         "C": -9, "C#": -8,
         "D": -7, "D#": -6,
@@ -33,8 +35,6 @@ def note_to_freq(note, decimal_places=3):
 
 
 def note_to_midi(note):
-    note_mapping = {'C': 0, 'C#': 1, 'D': 2, 'D#': 3, 'E': 4, 'F': 5, 'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10, 'B': 11}
-
     if len(note) < 2 or not note[0].isalpha():
         raise ValueError("Invalid input: enter a valid note (e.g. A4)")
 
@@ -47,11 +47,8 @@ def note_to_midi(note):
 
 
 def note_to_key_number(note):
-    note_mapping = {'C': 0, 'C#': 1, 'D': 2, 'D#': 3, 'E': 4, 'F': 5, 'F#': 6, 'G': 7, 'G#': 8, 'A': 9, 'A#': 10,
-                    'B': 11}
-
     if len(note) < 2 or not note[0].isalpha():
-        raise ValueError("Invalid input: enter a valid piano key (e.g. A4)")
+        raise ValueError("Invalid input: enter a valid note (e.g. A4)")
 
     note_name = note[:-1]
     octave = int(note[-1])
